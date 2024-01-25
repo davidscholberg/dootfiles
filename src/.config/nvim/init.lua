865e27ea66dff955aa4d5792e5c3b1c8067d602b
David Scholberg <recombinant.vector@gmail.com>
2024-01-25 17:00:16 -0500

    added initial neovim config

---
+++ src/.config/nvim/init.lua
@@ -0,0 +1,52 @@
+-- Remap leader to spacebar
+vim.g.mapleader = " "
+
+-- Enable relative line numbers
+vim.wo.relativenumber = true
+
+-- Bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup plugins
+require("lazy").setup({
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('vscode').load()
+    end,
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+})
+
+-- Configure telescope
+local telescope = require('telescope')
+telescope.setup({
+  pickers = {
+    find_files = {
+      hidden = true,
+      find_command = {'rg', '--files', '--color', 'never', '--glob', '!.git'},
+    }
+  }
+})
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})

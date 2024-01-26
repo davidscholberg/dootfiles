97286dd1f819c30ac891c7373f626f2b07067034
David Scholberg <recombinant.vector@gmail.com>
2024-01-26 18:09:45 -0500

    replaced single quotes with double quotes in lua configs

--- src/.config/nvim/lua/plugins/vscode.lua
+++ src/.config/nvim/lua/plugins/vscode.lua
@@ -4,7 +4,7 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
-            require('vscode').load()
+            require("vscode").load()
         end,
     }
 }
--- src/.var/app/org.wezfurlong.wezterm/config/wezterm/wezterm.lua
+++ src/.var/app/org.wezfurlong.wezterm/config/wezterm/wezterm.lua
@@ -1,16 +1,16 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- Set font
-config.font = wezterm.font('Monospace')
+config.font = wezterm.font("Monospace")
 config.font_size = 10.0
 
 -- Set color scheme
-config.color_scheme = 'Vs Code Dark+ (Gogh)'
+config.color_scheme = "Vs Code Dark+ (Gogh)"
 
 return config
 

7df75382fe31265de18362f41c6512125209c2c7
David Scholberg <recombinant.vector@gmail.com>
2024-01-25 23:01:35 -0500

    added treesitter to nvim config, reorganized a bit

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -19,8 +19,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Setup plugins
+-- Setup and configure plugins
 require("lazy").setup({
+
+  -- vscode theme
   {
     "Mofiqul/vscode.nvim",
     lazy = false,
@@ -29,25 +31,49 @@ require("lazy").setup({
       require('vscode').load()
     end,
   },
+
+  -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        ensure_installed = {
+          "bash", "c", "cmake", "cpp", "dockerfile", "git_config", "git_rebase",
+          "gitcommit", "gitignore", "javascript", "json", "lua", "make",
+          "markdown", "markdown_inline", "python", "ssh_config", "typescript",
+          "vim", "vimdoc",
+        },
+        sync_install = false,
+        highlight = { enable = true },
+      })
+    end
+  },
+
+  -- telescope
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+      local telescope = require('telescope')
+      telescope.setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+            find_command = {
+              'rg', '--files', '--color', 'never', '--glob', '!.git'
+            },
+          }
+        }
+      })
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
   },
 })
 
--- Configure telescope
-local telescope = require('telescope')
-telescope.setup({
-  pickers = {
-    find_files = {
-      hidden = true,
-      find_command = {'rg', '--files', '--color', 'never', '--glob', '!.git'},
-    }
-  }
-})
-local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})

2230a7c8ee832f7efa87977f74d53a69af950a9f
David Scholberg <recombinant.vector@gmail.com>
2024-01-30 15:13:58 -0500

    set custom window and tab titles for wezterm

--- src/.var/app/org.wezfurlong.wezterm/config/wezterm/sphericalcow_wezterm.lua
+++ src/.var/app/org.wezfurlong.wezterm/config/wezterm/sphericalcow_wezterm.lua
@@ -12,4 +12,24 @@ config.font_size = 10.0
 -- Set color scheme
 config.color_scheme = "Vs Code Dark+ (Gogh)"
 
+-- Set window title
+wezterm.on(
+    "format-window-title",
+    function (tab, pane, tabs, panes, config)
+        return ""
+    end
+)
+
+-- Set tab titles
+wezterm.on(
+    "format-tab-title",
+    function (tab, tabs, panes, config, hover, max_width)
+        local pane_title = tab.active_pane.title
+        return {
+            {Background = {Color = "#1e1e1e"}},
+            {Text = pane_title:sub(pane_title:find(":") + 1)},
+        }
+    end
+)
+
 return config

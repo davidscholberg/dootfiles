0812beb7bcaed54018a5093cc05214c130a9c38f
David Scholberg <recombinant.vector@gmail.com>
2024-03-10 16:48:16 -0400

    added interpreter logic to ExecuteCurrentFile

--- src/.config/nvim/lua/scripts/execute_current_file.lua
+++ src/.config/nvim/lua/scripts/execute_current_file.lua
@@ -4,6 +4,12 @@ local compile_table = {
     c = "gcc %s -o %s && %s",
 }
 
+-- Mapping of file extensions to interpret commands.
+-- The format strings must have 1 argument: the source path.
+local interpret_table = {
+    py = "python %s",
+}
+
 -- Executes the current source file with the compiler/interpreter defined for the file type.
 vim.api.nvim_create_user_command(
     "ExecuteCurrentFile",
@@ -14,10 +20,13 @@ vim.api.nvim_create_user_command(
 
         -- Get configured compile or interpret command for file type
         local command = nil
-        local command_fmt = compile_table[file_extension]
-        if command_fmt ~= nil then
+        if compile_table[file_extension] ~= nil then
             local exe_path = vim.fn.system("mktemp"):sub(1, -2)
+            local command_fmt = compile_table[file_extension]
             command = command_fmt:format(source_filepath, exe_path, exe_path)
+        elseif interpret_table[file_extension] ~= nil then
+            local command_fmt = interpret_table[file_extension]
+            command = command_fmt:format(source_filepath)
         else
             print(("error: file extension \"%s\" not supported for execution"):format(file_extension))
             return

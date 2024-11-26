bf2cdf3f02e3e9fa72c3566cf9b40c282a41ff61
David Scholberg <recombinant.vector@gmail.com>
2024-11-26 16:16:01 -0500

    added shlvl handlink for cmd, compensated for bash shlvl bug in starship

--- windows-src/AppData/Local/clink/starship.lua
+++ windows-src/AppData/Local/clink/starship.lua
@@ -1,1 +1,19 @@
+-- Manage SHLVL manually
+clink.oninject(function ()
+    shlvl = os.getenv("SHLVL")
+    if (not shlvl) then
+        -- This should be set to 1, but there's a bug with the shlvl handling in bash, so we compensate for it here.
+        os.setenv("SHLVL", "0")
+        return
+    end
+
+    shlvl_num = tonumber(shlvl)
+    if (not shlvl_num) then
+        return
+    end
+
+    os.setenv("SHLVL", tostring(shlvl_num + 1))
+end)
+
+-- Load starship config
 load(io.popen('starship init cmd'):read("*a"))()

484f9a5f7ea5a618a4ec32881d14b37e9bc752d8
David Scholberg <recombinant.vector@gmail.com>
2024-11-27 03:17:08 -0500

    set vscode theme to leave terminal colors alone

--- src/.config/nvim/lua/plugins/vscode.lua
+++ src/.config/nvim/lua/plugins/vscode.lua
@@ -4,6 +4,10 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            require("vscode").setup({
+                -- prevent theme from touching terminal colors
+                terminal_colors = false,
+            })
             require("vscode").load()
         end,
     }

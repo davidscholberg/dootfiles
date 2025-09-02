d204fc89a864e411795ace0da899e6c636fd1b77
David Scholberg <recombinant.vector@gmail.com>
2025-09-01 20:43:03 -0400

    added buffer list to telescope config

--- src/.config/nvim/lua/plugins/telescope.lua
+++ src/.config/nvim/lua/plugins/telescope.lua
@@ -30,9 +30,10 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set(
                 "n",
-                "<leader>fb",
+                "<leader>fc",
                 function ()
                     builtin.live_grep({search_dirs = {vim.fn.expand("%:p")}})
                 end,

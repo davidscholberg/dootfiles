3439c1695edc0fa2c51119e9ff4217ef9dca1ed3
David Scholberg <recombinant.vector@gmail.com>
2025-12-21 14:30:31 -0500

    added nvim pager mode for man and git diff

---
+++ src/.config/nvim/lua/include/pager/general.lua
@@ -0,0 +1,43 @@
+local colors = require("include.colors")
+
+vim.api.nvim_create_autocmd(
+    "BufWinEnter",
+    {
+        desc = "Set buffer to behave as a general pager",
+        callback = function(event)
+            vim.api.nvim_set_option_value("filetype", "", {buf = event.buf})
+            local win = vim.fn.bufwinid(event.buf)
+            vim.api.nvim_set_option_value("number", false, {win = win})
+            vim.api.nvim_set_option_value("relativenumber", false, {win = win})
+
+            -- Push buffer contents through term to render ANSI escape codes
+            local lines = vim.api.nvim_buf_get_lines(event.buf, 0, -1, false)
+            vim.api.nvim_buf_set_lines(event.buf, 0, -1, false, {})
+            vim.api.nvim_chan_send(vim.api.nvim_open_term(event.buf, {}), table.concat(lines, "\n"))
+
+            vim.api.nvim_set_option_value("modifiable", false, {buf = event.buf})
+
+            vim.keymap.set("n", "q", "<cmd>qa<cr>", { silent = true, buffer = event.buf })
+
+            require("lualine").setup({
+                sections = {
+                    lualine_a = {{
+                        "mode",
+                        fmt = function() return "pager" end,
+                        color = {bg = colors.light_grey, gui = "bold"},
+                    }},
+                    lualine_b = {},
+                    lualine_x = {},
+                    lualine_y = {{
+                        "progress",
+                        color = {fg = colors.light_grey},
+                    }},
+                    lualine_z = {{
+                        "location",
+                        color = {bg = colors.light_grey, gui = "bold"},
+                    }},
+                },
+            })
+        end
+    }
+)
